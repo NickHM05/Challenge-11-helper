ab25d6b6833a5630680783694413c58707b6ad27
NicholasHM05 <nhmamberger@gmail.com>
2023-01-14 21:48:23 -0600

    saving partial thoughts, comments, consts made, turning in for the night

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+//the express server used with node and is created.
 const app = express();
+// the port to use via http:localhost 3001 
 const PORT = process.env.PORT || 3001
+//This may be packages to get the correct file path for our html. Further testing is needed.
+const router = require('express').Router();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use("/api",apiRoutes);
 app.use("/",htmlRoutes);
+// here is the listener
 app.listen(PORT,()=>console.log(`listening on port; ${PORT}`));
